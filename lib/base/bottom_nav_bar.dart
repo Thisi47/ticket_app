3315440ec832f019fcd271557aae4dd456396724
Thiery Mbaigolmem Dang-Dang <thierrymbaye@gmail.com>
2024-08-09 07:40:47 +0100

    Second commit

---
+++ lib/base/bottom_nav_bar.dart
@@ -0,0 +1,62 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  final app_screens = <Widget>[
+    const Center(child: Text("Home")),
+    const Center(child: Text("Search")),
+    const Center(child: Text("Tickets")),
+    const Center(child: Text("Profile")),
+  ];
+
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child:  Text("My Ticket")),
+      ),
+      body: app_screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.blueGrey,
+        unselectedItemColor: const Color(0xFF526400),
+        showSelectedLabels: false,
+        onTap: _onItemTapped,
+        currentIndex: _selectedIndex,
+        items:const [
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+              label: "Home",
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled)),
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+              label: "Search",
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled)),
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+              label: "Tickets",
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled)),
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+              label: "Profile",
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled)),
+        ],
+      ),
+    );
+  }
+}
